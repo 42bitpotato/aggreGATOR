78e5cb7e95d755571915661f217a37cd5d56c802
42bitpotato <skickligt01.flagig@icloud.com>
2025-08-10 07:35:12 +0200

    get config file path function and constant

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,16 @@
 package config
+
+import (
+	"fmt"
+	"os"
+)
+
+const configFileName = ".gatorconfig.json"
+
+func getConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("unable to get home dir: %v", err)
+	}
+	return homeDir, nil
+}
