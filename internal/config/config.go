21c8f8900dc0552f636c38218ca12903efddc8cd
42bitpotato <skickligt01.flagig@icloud.com>
2025-08-11 06:51:21 +0200

    Setting up config file reader

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 const configFileName = ".gatorconfig.json"
@@ -12,10 +14,29 @@ func getConfigFilePath() (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("unable to get home dir: %v", err)
 	}
-	return homeDir, nil
+	filePath := filepath.Join(homeDir, configFileName)
+	if _, err := os.Stat(filePath); err == nil {
+		return homeDir, nil
+	} else if os.IsNotExist(err) {
+		return "", fmt.Errorf("unable to get config json: %v", err)
+	}
+	return "", fmt.Errorf("error getting config file path: %v", err)
 }
 
 type Config struct {
 	DbUrl           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
+}
+
+func ReadConfigJson() (Config, error) {
+	jsonFile, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+	var config Config
+	decoder = json.NewDecoder(jsonFile)
+	if err := decoder.Decode(&config); err != nil {
+		return nil, fmt.Errorf("unable to decode config json: %v", err)
+	}
+	return config, nil
 }
