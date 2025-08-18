882473fad8ab0145b00b23612e40448d00fa7789
42bitpotato <skickligt01.flagig@icloud.com>
2025-08-18 06:22:45 +0200

    implementing commands

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,11 +16,29 @@ func getConfigFilePath() (string, error) {
 	}
 	filePath := filepath.Join(homeDir, configFileName)
 	if _, err := os.Stat(filePath); err == nil {
-		return homeDir, nil
+		return filePath, nil
 	} else if os.IsNotExist(err) {
 		return "", fmt.Errorf("unable to get config json: %v", err)
 	}
 	return "", fmt.Errorf("error getting config file path: %v", err)
+}
+
+func write(cfg *Config) error {
+	jsonFilePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+	jsonFile, err := os.Create(jsonFilePath)
+	if err != nil {
+		return err
+	}
+	defer jsonFile.Close()
+
+	err = json.NewEncoder(jsonFile).Encode(cfg)
+	if err != nil {
+		return fmt.Errorf("failed to write config to json: %v", err)
+	}
+	return nil
 }
 
 type Config struct {
@@ -38,25 +56,21 @@ func Read() (Config, error) {
 	if err != nil {
 		return Config{}, err
 	}
+	defer jsonFile.Close()
 
 	var cfg Config
 	decoder := json.NewDecoder(jsonFile)
 	if err := decoder.Decode(&cfg); err != nil {
-		return nil, fmt.Errorf("unable to decode config json: %v", err)
+		return Config{}, fmt.Errorf("unable to decode config json: %v", err)
 	}
 	return cfg, nil
 }
 
-func SetUser(cfg Config, username string) error {
+func SetUser(cfg *Config, username string) error {
 	cfg.CurrentUserName = username
-	filePath, err := getConfigFilePath()
+	err := write(cfg)
 	if err != nil {
-		return err
-	}
-
-	err = json.NewEncoder(filePath).Encode(cfg)
-	if err != nil {
-		return fmt.Errorf("failed to set username in config json: %v", err)
+		return fmt.Errorf("failed to write username to config json: %v", err)
 	}
 	return nil
 }
--- main.go
+++ main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/42bitpotato/aggreGATOR/internal/config"
 )
 
-func main() error {
+type State struct {
+	cfg *config.Config
+}
+
+func main() {
+	var cfg config.Config
 	cfg, err := config.Read()
 	if err != nil {
-		return err
+		fmt.Println("error reading config:", err)
+		os.Exit(1)
 	}
-	err = config.SetUser(cfg, "Macke")
+	err = config.SetUser(&cfg, "Macke")
 	if err != nil {
-		return nil
+		fmt.Println("error setting user:", err)
+		os.Exit(1)
 	}
-	cfg, err = config.Read()
-	if err != nil {
-		return err
-	}
-	fmt.Print(cfg)
-	return nil
+	cfg2, _ := config.Read()
+	cfgJson, _ := json.Marshal(cfg2)
+	fmt.Println(string(cfgJson))
 }
