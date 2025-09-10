483fbfc5dc4c5ea609e7425e1560c7d5907aed3c
42bitpotato <skickligt01.flagig@icloud.com>
2025-09-10 07:25:51 +0200

    Added feed_follows table

---
+++ sql/schema/003_feed_follows.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+CREATE TABLE feed_follows (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id UUID NOT NULL,
+    feed_id UUID NOT NULL,
+
+    CONSTRAINT fk_user_id
+    FOREIGN KEY (user_id)
+    REFERENCES users(id),
+
+    CONSTRAINT fk_feed_id
+    FOREIGN KEY (feed_id)
+    REFERENCES feeds(id),
+
+    CONSTRAINT unique_user_feed
+    UNIQUE (user_id, feed_id)
+);
+
+-- +goose Down
+DROP TABLE feed_follows;
