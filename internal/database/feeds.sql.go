181cca246cc4b23db4fdf1cabb826704555d3b02
42bitpotato <skickligt01.flagig@icloud.com>
2025-09-11 10:09:28 +0200

    generated new query

--- internal/database/feeds.sql.go
+++ internal/database/feeds.sql.go
@@ -45,6 +45,58 @@ func (q *Queries) AddFeed(ctx context.Context, arg AddFeedParams) error {
 	return err
 }
 
+const createFeedFollow = `-- name: CreateFeedFollow :one
+WITH inserted_feed_follow AS (
+    INSERT INTO feed_follows (user_id, feed_id)
+    VALUES (
+        $1,
+        $2
+    )
+    RETURNING id, created_at, updated_at, user_id, feed_id
+)
+SELECT 
+    inserted_feed_follow.id,
+    inserted_feed_follow.created_at,
+    inserted_feed_follow.updated_at,
+    inserted_feed_follow.user_id,
+    inserted_feed_follow.feed_id, 
+    users.name AS user_name,
+    feeds.name AS feed_name
+FROM inserted_feed_follow
+INNER JOIN users ON inserted_feed_follow.user_id = users.id
+INNER JOIN feeds ON inserted_feed_follow.feed_id = feeds.id
+`
+
+type CreateFeedFollowParams struct {
+	UserID uuid.UUID
+	FeedID uuid.UUID
+}
+
+type CreateFeedFollowRow struct {
+	ID        uuid.UUID
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	UserID    uuid.UUID
+	FeedID    uuid.UUID
+	UserName  string
+	FeedName  string
+}
+
+func (q *Queries) CreateFeedFollow(ctx context.Context, arg CreateFeedFollowParams) (CreateFeedFollowRow, error) {
+	row := q.db.QueryRowContext(ctx, createFeedFollow, arg.UserID, arg.FeedID)
+	var i CreateFeedFollowRow
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.UserID,
+		&i.FeedID,
+		&i.UserName,
+		&i.FeedName,
+	)
+	return i, err
+}
+
 const getFeed = `-- name: GetFeed :one
 SELECT id, created_at, updated_at, name, url, user_id FROM feeds
 WHERE name = $1
