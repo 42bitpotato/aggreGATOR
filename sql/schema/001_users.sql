941932bcab2379b468fc3fb8ed2d857acb79d33d
42bitpotato <skickligt01.flagig@icloud.com>
2025-09-14 09:20:23 +0200

    removed timezone

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,8 +3,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
     name TEXT UNIQUE NOT NULL
 );
 
--- sql/schema/004_feed_last_fetched.sql
+++ sql/schema/004_feed_last_fetched.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 ALTER TABLE feeds
-ADD COLUMN last_fetched_at TIMESTAMPTZ NULL;
+ADD COLUMN last_fetched_at TIMESTAMP NULL;
 
 -- +goose Down
 ALTER TABLE feeds
