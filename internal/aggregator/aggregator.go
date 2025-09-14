e8272ab6ef097fcba13271c8264824f1f535fe41
42bitpotato <skickligt01.flagig@icloud.com>
2025-09-14 21:56:22 +0200

    Adjusted ScrapeFeed to save posts to db

--- internal/aggregator/aggregator.go
+++ internal/aggregator/aggregator.go
@@ -2,10 +2,18 @@ package aggregator
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/42bitpotato/aggreGATOR/internal/config"
+	"github.com/42bitpotato/aggreGATOR/internal/database"
 	"github.com/42bitpotato/aggreGATOR/internal/rss"
+	"github.com/araddon/dateparse"
+	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
 
 func ScrapeFeeds(s *config.State) error {
@@ -24,11 +32,49 @@ func ScrapeFeeds(s *config.State) error {
 		return fmt.Errorf("error fetching RSS feed: %v", err)
 	}
 
-	fmt.Printf("----------\nFETCHED FEED: %s\nITEMS:\n", feed.Channel.Title)
-	for _, item := range feed.Channel.Item {
-		fmt.Printf("*	%s\n", item.Title)
+	for _, post := range feed.Channel.Item {
+
+		var pubDate sql.NullTime
+		parsedDate, err := dateparse.ParseAny(post.PubDate)
+		if err != nil {
+			s.Logger.Printf("could not parse pubDate %q: %v", post.PubDate, err)
+			pubDate = sql.NullTime{Valid: false}
+		} else {
+			pubDate = sql.NullTime{Time: parsedDate, Valid: true}
+		}
+
+		postArgs := database.CreatePostParams{
+			ID:           uuid.New(),
+			CreatedAt:    time.Now(),
+			UpdatedAt:    time.Now(),
+			Title:        post.Title,
+			Url:          post.Link,
+			Description:  post.Description,
+			PublishedAt:  pubDate,
+			PublishedRaw: post.PubDate,
+			FeedID:       nextFeed.ID,
+		}
+
+		// Retry 3 times if error
+		for i := 0; i < 3; i++ {
+			err = s.Db.CreatePost(context.Background(), postArgs)
+			if err != nil {
+				retry, err := handleCreatePostErr(err)
+				if retry {
+					continue
+				} else if err != nil {
+					s.Logger.Printf("error creating post: %v", err)
+				}
+			}
+			break
+		}
 	}
-	fmt.Print("\n")
+
+	//fmt.Printf("----------\nFETCHED FEED: %s\nITEMS:\n", feed.Channel.Title)
+	//for _, item := range feed.Channel.Item {
+	//	fmt.Printf("*	%s\n", item.Title)
+	//}
+	//fmt.Print("\n")
 
 	return nil
 }
@@ -42,4 +88,20 @@ func fetchFeed(url string) (feed *rss.RSSFeed, err error) {
 	}
 
 	return Feed, nil
+}
+
+func handleCreatePostErr(in error) (retry bool, out error) {
+	if in == nil {
+		return false, nil
+	}
+	var pqErr *pq.Error
+	code := string(pqErr.Code)
+	if errors.As(in, &pqErr) {
+		if code == "23505" && pqErr.Constraint == "posts_url_key" {
+			return false, nil
+		} else if strings.HasPrefix(code, "08") {
+			return true, in
+		}
+	}
+	return false, in
 }
