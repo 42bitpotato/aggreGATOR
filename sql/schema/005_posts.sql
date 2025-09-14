90b3cc60d96f47073aa97dc035321152173000da
42bitpotato <skickligt01.flagig@icloud.com>
2025-09-14 12:21:50 +0200

    Added posts table to sql db

---
+++ sql/schema/005_posts.sql
@@ -0,0 +1,21 @@
+-- +goose Up
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE posts (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    title TEXT NOT NULL,
+    url TEXT UNIQUE NOT NULL,
+    description TEXT,
+    published_at TEXT,
+    feed_id UUID NOT NULL,
+
+    CONSTRAINT fk_feed_id
+    FOREIGN KEY (feed_id)
+    REFERENCES feeds(id)
+    ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE posts;
